210db918a21f492f7746dca80afd4fa9494fd01c
anapedra <anapedra.mil@gmail.com>
2024-04-12 21:59:05 -0300

    Project dio-avengers-api created

---
+++ backend/dio-avengers-api/src/test/kotlin/com/anapedra/dioavengersapi/DioAvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.anapedra.dioavengersapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DioAvengersApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
